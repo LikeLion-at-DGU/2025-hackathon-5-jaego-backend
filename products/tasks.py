ed5dd8357668a240a1c5b46bfe3be5c3135c568a
Yeonhee-Ha <flfkrdl123@gmail.com>
2025-08-22 22:47:49 +0900

    [#94] deploy: 상품 업데이트시 임베딩 삭제

--- products/tasks.py
+++ products/tasks.py
@@ -15,19 +15,19 @@ def deactivate_expired_products():
 
 #####################################################################
 #단일 상품 등록/수정 시 임베딩 생성
-@shared_task
-def create_embedding_for_product(product_id):
-    try:
-        product = Product.objects.get(id=product_id, is_active=True)
-        build_embedding_for_product(product)  # 개별 상품 임베딩 생성
-        # 임베딩 메모리도 즉시 갱신
-        from accounts.services.reco import ITEM_VECS, ITEM_IDS, IDX, load_item_vectors, load_item_ids
-        ITEM_VECS[:] = load_item_vectors()
-        ITEM_IDS[:] = load_item_ids()
-        IDX.clear()
-        IDX.update({int(pid): i for i, pid in enumerate(ITEM_IDS)})
-    except Product.DoesNotExist:
-        pass
+# @shared_task
+# def create_embedding_for_product(product_id):
+#     try:
+#         product = Product.objects.get(id=product_id, is_active=True)
+#         build_embedding_for_product(product)  # 개별 상품 임베딩 생성
+#         # 임베딩 메모리도 즉시 갱신
+#         from accounts.services.reco import ITEM_VECS, ITEM_IDS, IDX, load_item_vectors, load_item_ids
+#         ITEM_VECS[:] = load_item_vectors()
+#         ITEM_IDS[:] = load_item_ids()
+#         IDX.clear()
+#         IDX.update({int(pid): i for i, pid in enumerate(ITEM_IDS)})
+#     except Product.DoesNotExist:
+#         pass
 
 @shared_task
 def daily_embedding_refresh():
