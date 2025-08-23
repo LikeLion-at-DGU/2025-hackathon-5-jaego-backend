c3ff74b5908340b2ed212827fa1355d179522cc4
Yeonhee-Ha <flfkrdl123@gmail.com>
2025-08-24 00:35:13 +0900

    [#95] refac : str 수정

--- reservations/models.py
+++ reservations/models.py
@@ -73,4 +73,4 @@ class Notification(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return f"[{self.id}] - ({self.reservation.id}){self.status}({'읽음' if self.is_read else '안읽음'})"
+        return f"[{self.id}] - 예약번호 ({self.reservation.id}) / 상태 : {self.status} / ({'읽음' if self.is_read else '안읽음'})"
